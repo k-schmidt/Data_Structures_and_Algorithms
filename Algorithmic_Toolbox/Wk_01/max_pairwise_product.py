7c0d7adb263268c0fc0eff101c3d78342292b264
Kyle Schmidt <kas2207@columbia.edu>
2016-04-08 16:14:56 -0400

    beginning week 2

--- Algorithmic_Toolbox/Wk_01/max_pairwise_product.py
+++ Algorithmic_Toolbox/Wk_01/max_pairwise_product.py
@@ -1,23 +1,46 @@
 # Uses python3
+import random
+
 n = int(input())
 a = [int(x) for x in input().split()]
 assert(len(a) == n)
 
-result = 0
-n = 10000000
-a = [random.randint(1, 1000000000000000000000) for x in xrange
+def max_pairwise_one(list_nums):
+    max_num1 = 0
+    max_num2 = 0
+    for i in list_nums:
+        if max_num1 == 0 or i > max_num1:
+            max_num1 = i
 
-def max_pairwise_one(max=n):
-    while i < n:
-        a = [random.randint(1,1000000000000) for x in xrange(i)]
-        max1 = a.pop(a.index(max(a)))
-        max2 = a.pop(a.index(max(a)))
+    if list_nums.count(max_num1) > 1:
+        max_num2 = max_num1
+    else:
+        for j in list_nums:
+            if j > max_num2 and j != max_num1:
+                max_num2 = j
+    return max_num1 * max_num2
 
-def max_pairwise_two(max=n):
-    for i in range(0, n):
-        for j in range(i+1, n):
-            if a[i]*a[j] > result:
-                result = a[i]*a[j]
-                yield result
+def max_pairwise_two(list_nums):
+    result = 0
+    for i in range(len(list_nums)):
+        for j in range(len(list_nums)):
+            if a[i] * a[j] > result:
+                result = a[i] * a[j]
+    return result
 
-print(result)
+"""
+while True:
+    n = random.randint(1,10)
+    print(n)
+    a = [random.randint(1, 10) for x in range(n)]
+    print(a)
+    if max_pairwise_one(a) != max_pairwise_two(a):
+        print("Wrong Answer: {0} {1}".format(max_pairwise_one(a),
+                                             max_pairwise_two(a)))
+        break
+    else:
+        print("OK")
+"""
+
+
+print(max_pairwise_one(a))
